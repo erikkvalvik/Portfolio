6d36f20aa2e81e6e45cbad21ca63ca10fe3fd0b8
erikkvalvik <erik.kvalvik@gmail.com>
2021-10-07 14:55:12 +0200

    adding routing and styling to components

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -4,12 +4,33 @@ import React from "react";
 import GlobalStyle from "./components/GlobalStyle";
 //Import Pages
 import AboutUs from "./pages/AboutUs";
+import ContactUs from "./pages/ContactUs";
+import OurWork from "./pages/OurWork";
+import Nav from "./components/Nav";
+import MovieDetail from "./pages/MovieDetail";
+//Router
+import {Switch, Route} from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
-      <AboutUs/>
+      <Nav/>
+      <Switch>
+        <Route path="/" exact>
+          <AboutUs/>
+        </Route>
+        <Route path="/work" exact>
+          <OurWork/>
+        </Route>
+        <Route path="/work/:id">
+          <MovieDetail />
+        </Route>
+        <Route path="/contact">
+          <ContactUs/>
+        </Route>
+      </Switch>
     </div>
   );
 }
--- portfolio/src/components/FaqSection.js
+++ portfolio/src/components/FaqSection.js
@@ -1,8 +1,11 @@
 import React from "react";
+import styled from "styled-components";
+import {About} from "../styles";
+
 
 const FaqSection = () => {
     return(
-        <div className="faq">
+        <Faq>
             <h2>Any questions? <span>FAQ</span></h2>
             <div className="question">
                 <h4>How Do I Start?</h4>
@@ -10,6 +13,7 @@ const FaqSection = () => {
                     <p>Lorem ipsum dolor sit amet.</p>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, adipisci?</p>
                 </div>
+                <div className="faq-line"></div>
             </div>
             <div className="question">
                 <h4>Daily Schedule</h4>
@@ -17,6 +21,7 @@ const FaqSection = () => {
                     <p>Lorem ipsum dolor sit amet.</p>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, adipisci?</p>
                 </div>
+                <div className="faq-line"></div>
             </div>
             <div className="question">
                 <h4>Different Payment Methods</h4>
@@ -24,6 +29,7 @@ const FaqSection = () => {
                     <p>Lorem ipsum dolor sit amet.</p>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, adipisci?</p>
                 </div>
+                <div className="faq-line"></div>
             </div>
             <div className="question">
                 <h4>What Products do you offer?</h4>
@@ -31,10 +37,42 @@ const FaqSection = () => {
                     <p>Lorem ipsum dolor sit amet.</p>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, adipisci?</p>
                 </div>
+                <div className="faq-line"></div>
             </div>
-        </div>
+        </Faq>
 
     )
 }
+
+const Faq = styled(About)`
+    display: block;
+    span{
+        display: block;
+
+    }
+    h2{
+        padding-bottom: 2rem;
+        font-weight: lighter;
+    }
+    .faq-line{
+        background: #cccccc;
+        height: 0.2rem;
+        margin: 2rem 0rem;
+        width: 100%;
+
+    }
+
+    .question{
+        padding: 3rem 0rem;
+        cursor: pointer;
+
+    }
+    .answer{
+        padding: 2rem 0rem;
+        p{
+            padding: 1rem 0rem;
+        }
+    }
+`;
 
 export default FaqSection;
