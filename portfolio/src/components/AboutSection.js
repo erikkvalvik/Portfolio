9f1bbe33eee9023a1e56fe38885152fcb98af94e
erikkvalvik <erik.kvalvik@gmail.com>
2021-10-11 13:50:02 +0200

    imported framer-motion

--- portfolio/src/components/AboutSection.js
+++ portfolio/src/components/AboutSection.js
@@ -1,8 +1,10 @@
 import React from "react";
 import home1 from '../img/home1.png';
-//Styled
-import styled from "styled-components";
 import {About, Description, Image, Hide} from "../styles";
+//Framer Motion
+import {motion} from "framer-motion";
+
+
 
 const AboutSection = () => {
     return(
@@ -10,7 +12,7 @@ const AboutSection = () => {
             <Description>
                 <div className="title">
                     <Hide>
-                        <h2>We work to make</h2>
+                        <motion.h2>We work to make</motion.h2>
                     </Hide>
                     <Hide>
                         <h2>your <span>dreams</span> come</h2>
